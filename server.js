1d9aa72594eace70b78371085406b687998e9daf
MarcinBalejko <mbbm9@o2.pl>
2020-04-06 23:09:22 +0200

    :recycle: Emitting welcome message

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static(path.join(__dirname, "public")));
 // Run when client connects
 io.on("connection", (socket) => {
   console.log("New Websocket Connection");
+
+  socket.emit("message", "Welcome to TalkyTalk!");
 });
 
 const PORT = process.env.PORT || 3000;
