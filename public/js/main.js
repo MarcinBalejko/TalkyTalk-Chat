ceea4136592e169a4f662b5d0a44413261e721ad
MarcinBalejko <mbbm9@o2.pl>
2020-04-07 19:09:55 +0200

    :white_check_mark: message gets output to the chat

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@ const chatForm = document.getElementById("chat-form");
 
 const socket = io();
 
+// Message from server
 socket.on("message", (message) => {
   console.log(message);
+  outputMessage(message);
 });
 
 // Message submit
@@ -16,3 +18,14 @@ chatForm.addEventListener("submit", (e) => {
   // Emit message to server
   socket.emit("chatMessage", msg);
 });
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+  <p class="text">
+    ${message}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
