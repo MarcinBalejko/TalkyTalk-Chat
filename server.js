8f8aed021b7e73ac0873b1b34b8656f40211b803
MarcinBalejko <mbbm9@o2.pl>
2020-04-08 02:17:50 +0200

    :white_check_mark: Users can join the chat

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -15,25 +16,31 @@ const botName = "TalkyTalk Bot";
 
 // Run when client connects
 io.on("connection", (socket) => {
-  console.log("New Websocket Connection");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  // Welcome current user
-  socket.emit("message", formatMessage(botName, "Welcome to TalkyTalk!"));
+    socket.join(user.room);
 
-  // Broadcast when a user connects
-  socket.broadcast.emit(
-    "message",
-    formatMessage(botName, "A user has joined the chat")
-  );
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to TalkyTalk!"));
 
-  // Runs when client disconnects
-  socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, "A user has left the chat"));
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
   });
 
   // Listen for chat message
   socket.on("chatMessage", (msg) => {
     io.emit("message", formatMessage("USER", msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 });
 
