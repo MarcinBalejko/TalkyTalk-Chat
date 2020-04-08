97201961d0c8b0411a997bb4457389f637945c8f
MarcinBalejko <mbbm9@o2.pl>
2020-04-08 17:52:19 +0200

    :white_check_mark: Adding room name to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -10,6 +11,12 @@ const socket = io();
 
 // Join chatroom
 socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 // Message from server
 socket.on("message", (message) => {
@@ -44,4 +51,9 @@ function outputMessage(message) {
     ${message.text}
   </p>`;
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
 }
--- server.js
+++ server.js
@@ -38,6 +38,12 @@ io.on("connection", (socket) => {
       );
   });
 
+  // Send users and room info
+  io.to(user.room).emit("roomUsers", {
+    room: user.room,
+    users: getRoomUsers(user.room),
+  });
+
   // Listen for chat message
   socket.on("chatMessage", (msg) => {
     const user = getCurrentUser(socket.id);
@@ -54,6 +60,12 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     }
   });
 });
